a761a1a52d013934e824205f0ef01a39afc6a8c3
Rick Allen Herbert <219014337@mycput.ac.za>
2021-04-19 05:32:38 +0200

    Updated the Encryption And Decryption functions.

--- src/com/password_protection/Encryption.java
+++ src/com/password_protection/Encryption.java
@@ -1,6 +1,7 @@
 package com.password_protection;
 
 import javax.crypto.*;
+import java.nio.charset.StandardCharsets;
 
 public class Encryption {
 
@@ -9,8 +10,33 @@ public class Encryption {
             KeyGenerator keyGenerator = KeyGenerator.getInstance(encryption);
             SecretKey secretKey = keyGenerator.generateKey();
             return secretKey;
+        } catch (Exception e) {
+            return null;
         }
-        catch (Exception e) {
+    }
+
+    public byte[] encryptData(String data, SecretKey secretKey, Cipher cipher) {
+        try {
+            byte[] text = data.getBytes(StandardCharsets.UTF_8);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] textEncrypted = cipher.doFinal(text);
+
+            System.out.println("ENCRYPTED PASSWORD: " + textEncrypted);
+            return textEncrypted;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public String decryptData(byte[] data, SecretKey secretKey, Cipher cipher) {
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            byte[] textDecrypted = cipher.doFinal(data);
+            String output = new String(textDecrypted);
+
+            System.out.println("DECRYPTED PASSWORD: " + output);
+            return output;
+        } catch (Exception e) {
             return null;
         }
     }
--- src/com/password_protection/RunEncryption.java
+++ src/com/password_protection/RunEncryption.java
@@ -1,8 +1,32 @@
 package com.password_protection;
 
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import java.util.Scanner;
+
 public class RunEncryption {
 
     public static void main(String args[]) {
+        Encryption encryption = new Encryption();
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("ENTER CUSTOMER NAME: ");
+        String username = input.nextLine();
+        System.out.print("ENTER YOUR PASSWORD: ");
+        String password = input.nextLine();
+
+        try {
+            SecretKey secretKey = encryption.generateKey("AES");
+            Cipher cipher = Cipher.getInstance("AES");
+
+            byte[] encryptedData = encryption.encryptData(password, secretKey, cipher);
+            encryption.decryptData(encryptedData, secretKey, cipher);
+        }
+        catch (Exception e) {
+
+        }
+
+
 
     }
 }
